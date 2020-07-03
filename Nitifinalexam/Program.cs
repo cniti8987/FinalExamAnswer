6f24e0ca651bf408c199190f1785588964e711d7
niti <cniti8990@gmail.com>
2020-07-02 20:55:37 -0400

    my original project

---
+++ Nitifinalexam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Nitifinalexam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Audit a = new Audit();
+            Child c = new Child();
+
+            a.FirstName = "Nitiksha";
+            a.LastName = "Patel";
+
+            c.FirstName = "Amit";
+            c.LastName = "Amin";
+
+
+        }
+
+        public class Audit
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+        }
+       public  class Child
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+        }
+    }
+}
